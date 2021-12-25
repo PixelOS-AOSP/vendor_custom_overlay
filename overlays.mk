f9cff6b26a760adffedcf1d60bb0366db9fb9044
Karan Parashar <whyredfire@gmail.com>
2021-12-25 19:17:40 +0530

    overlays: Copy pixel exclusive xml correctly

--- overlays.mk
+++ overlays.mk
@@ -6,7 +6,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/common
 
 # Pixel sysconfig from Pixel XL (Photos)
 PRODUCT_COPY_FILES += \
-    vendor/overlays/prebuilt/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml \
+    vendor/overlays/prebuilt/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
 
 # RRO Overlays
 PRODUCT_PACKAGES += \
