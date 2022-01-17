cbc447313774275edab424861ce13591b8e56493
Karan Parashar <whyredfire@gmail.com>
2022-01-17 11:20:03 +0530

    overlays: Add sysconfig for QuickTap

--- overlays.mk
+++ overlays.mk
@@ -4,6 +4,10 @@ DEVICE_PACKAGE_OVERLAYS += vendor/overlays/common
 # Allow overlays to be excluded from enforcing RRO
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/common
 
+# Pixel sysconfig for QuickTap
+PRODUCT_COPY_FILES += \
+    vendor/overlays/prebuilt/etc/sysconfig/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
+
 # RRO Overlays
 PRODUCT_PACKAGES += \
     CustomPixelLauncherOverlay \
